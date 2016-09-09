f2bbbf54c94e8d7997ef4b150f315b07375caaaf
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-09-09 13:15:13 +0200

    removed mail verification

--- src/main/java/org/nextrtc/examples/videochat_with_rest/service/RegisterUserService.java
+++ src/main/java/org/nextrtc/examples/videochat_with_rest/service/RegisterUserService.java
@@ -1,20 +1,14 @@
 package org.nextrtc.examples.videochat_with_rest.service;
 
+import lombok.extern.log4j.Log4j;
 import org.nextrtc.examples.videochat_with_rest.domain.User;
 import org.nextrtc.examples.videochat_with_rest.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.sun.mail.util.MailConnectException;
-
 import javax.transaction.Transactional;
-
 import java.util.UUID;
-
-import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Service
@@ -23,8 +17,6 @@ public class RegisterUserService {
 
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     @Autowired
     private PasswordEncoder encoder;
@@ -43,23 +35,8 @@ public class RegisterUserService {
 
         User user = new User(username, encoder.encode(password), email, confirmationKey);
 
-        //TODO: call async
-        try {
-        	javaMailSender.send(prepareMail(user));
-        } catch (Throwable mailIssue ) {
-        	log.warn("Unable to send registration confirmation email", mailIssue);
-        }
+        //TODO: send email
 
         userRepository.save(user);
-    }
-
-    //TODO: move to service, parameters from properties
-    private SimpleMailMessage prepareMail(User user) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("no-reply@nextrtc.org");
-        message.setTo(user.getEmail());
-        message.setSubject("Account verification");
-        message.setText("enter site: /action/verify/" + user.getConfirmationKey());
-        return message;
     }
 }
