9da332083c13ac975cebdeb5fa77d65e873a0ae1
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-11-06 10:40:48 +0100

    fixed name displaying

--- src/main/java/org/nextrtc/examples/videochat_with_rest/controller/CheckMyName.java
+++ src/main/java/org/nextrtc/examples/videochat_with_rest/controller/CheckMyName.java
@@ -1,5 +1,7 @@
 package org.nextrtc.examples.videochat_with_rest.controller;
 
+import org.nextrtc.examples.videochat_with_rest.auth.AuthUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,11 @@ import java.security.Principal;
 @RequestMapping("/action/check")
 public class CheckMyName {
 
+    @Autowired
+    private AuthUtils authUtils;
+
     @RequestMapping(value = "name", method = RequestMethod.GET)
-    public String verify(Principal authentication) {
-        return authentication.getName();
+    public String check(Principal authentication) {
+        return authUtils.getAuthenticatedUser(authentication).getUsername();
     }
 }
